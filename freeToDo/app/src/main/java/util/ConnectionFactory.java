2fcc6143bd6ec8a6e759a635afcba935c8123650
Nadyne Madureira <madureira.nadyne@gmail.com>
2022-04-08 12:46:23 -0300

    Starting project MVC

---
+++ freeToDo/app/src/main/java/util/ConnectionFactory.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class ConnectionFactory {
+    
+    public static final String DRIVER = "com.mysql.jdbc.Driver";
+    public static final String URL = "jdbc:mysql://localhost:3306/freeToDo";
+    public static final String USER = "root";
+    public static final String PASS = "";
+
+    public static Connection getConnection(){
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex){
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection){
+        try{
+            if (connection != null){
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao encerrar a conexão com o banco de dados");
+        }
+    }
+    
+    public static void closeConnection(Connection connection, PreparedStatement statement){
+        try{
+            if (connection != null){
+                connection.close();
+            }
+            if (statement != null){
+                statement.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro ao encerrar a conexão com o banco de dados");
+        }
+    }
+}
